f04af8c9e64160429bce35f6a66570a719577fda
ameyamahajan <ameya.mahajan@sjsu.edu>
2014-07-10 21:15:01 -0700

    Mongo Connection

--- scripts/testFacebook.py
+++ scripts/testFacebook.py
@@ -1,19 +1,16 @@
 import requests
 import json
 import sys
+from pymongo import MongoClient
 
 base_url = 'https://graph.facebook.com/me'
 
-
-#ACCESS_TOKEN='250848248435335|mNpexGjKJ9um-xU5Vbr65O_kHKQ' 
-#ACCESS_TOKEN='CAADkJSi9kocBAGPdKseCn9kebsjfEZAXlTZAOIvURPP3pU3xObZBW8HPI0tQhNdmGJOZBxkbZAWrRZCbMSpLaaiOwnZCZClzwFbuqXUZAZCX6fpvjGtjdSACHOEwYXf3lqnrJZAVKOIqRZA9uv2sjGl0jBkZCyEgLZBJKpf8hZBVzFW6hhHebe2e0EOjxMzxXDIZCIIMVONokeY5Kyb99gZDZD'
-ACCESS_TOKEN='CAACEdEose0cBAEaYZCxZBdpbchTg6neZAiM8v2WCTqA2lKYb4LFnZCUAzdQFnWWVRhFcMPWjAcV9FNJEbmAQ3AfsklWf6IEZAkczsGxdG3hDBlGEcttNgJKeiMzyLfSeRGZCHhtOUln4NIpm9kLHZBOcHBnA2vTmTFwvm3ZBZB1HP9gclpavZAZC8cZAiGdAlMv5xkZCHThBZCvFq16wZDZD'
+ACCESS_TOKEN=''+sys.argv[2]
 
 # Get 10 likes for 10 friends
 fields = ''+sys.argv[1]
 
-url = '%s?fields=%s&access_token=%s' % \
-    (base_url, fields, ACCESS_TOKEN,)
+url = '%s?fields=%s&access_token=%s' % (base_url, fields, ACCESS_TOKEN,)
 
 # This API is HTTP-based and could be requested in the browser,
 # with a command line utlity like curl, or using just about
@@ -26,5 +23,12 @@ print url
 # to Python data structures
 content = requests.get(url).json()
 
+
+
 # Pretty-print the JSON and display it
-print json.dumps(content,indent=1)
+# print json.dumps(content,indent=1)
+
+client = MongoClient('localhost',27017)
+db = client.pda
+tab = db.ameya
+tab.insert(content)
