58bf33e431e3c68ed1bc2e12fbb2a7813c179f4d
ameyamahajan <ameya.mahajan@sjsu.edu>
2014-07-15 01:17:48 -0700

    Filter condition and count for male / females

--- src/DatabaseConnection/AggregationQuery.java
+++ src/DatabaseConnection/AggregationQuery.java
@@ -1,20 +1,49 @@
-// create our pipeline operations, first with the $match
-DBObject match = new BasicDBObject("$match", new BasicDBObject("type", "airfare"));
+package DatabaseConnection;
 
-// build the $projection operation
-DBObject fields = new BasicDBObject("gender", 1);
-fields.put("countr", 1);
-fields.put("_id", 0);
-DBObject project = new BasicDBObject("$project", fields );
+import java.util.ArrayList;
+import java.util.List;
 
-// Now the $group operation
-DBObject groupFields = new BasicDBObject( "_id", "$gender");
-groupFields.put("count", new BasicDBObject( "$cnt", "$countr"));
-DBObject group = new BasicDBObject("$group", groupFields);
 
-// Finally the $sort operation
-DBObject sort = new BasicDBObject("$sort", new BasicDBObject("countr", -1));
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.BasicDBObject;
+import com.mongodb.AggregationOutput;
 
-// run aggregation
-List<DBObject> pipeline = Arrays.asList(project, group, sort);
-AggregationOutput output = coll.aggregate(pipeline);
+
+
+public class AggregationQuery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MongoClient mongo = MongoSingleton.INSTANCE.getMongo();
+		DB db = mongo.getDB("pda");
+		DBCollection user = db.getCollection("friends");
+		
+		/*Look for just the filter, in this case where gender='female' */
+		BasicDBObject query = new BasicDBObject("gender", "female"); 
+		DBCursor cursor = user.find(query);
+		try {
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
+
+		} finally {
+			cursor.close();
+		}
+
+		
+		
+		/*Agg queries to get Male / Female count */
+		System.out.println("Male friends "+user.count(new BasicDBObject("gender","male")));
+		System.out.println("Female friends  "+user.count(new BasicDBObject("gender","female")));
+
+
+
+
+
+	}
+
+}
+
